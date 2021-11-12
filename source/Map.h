bf8ed7274e8b9fb1e2b1ba34a77e3acb1fcf3acd
ssw**** <ssw3095@naver.com>
2021-11-12 14:03:29 +0000

    update: Main story is changed
    <description>
    - delete "Structure.h"
    - delete "Room.h"
    - delete "Passage.h"
    Because the way of playing a game is changed, I deleted
    some classes. The changed map will be a type of maze.
    It will give player more fun.
    <issue-number>

--- source/Map.h
+++ source/Map.h
@@ -1,32 +1,58 @@
 /*
-생각해보니 Room은 타일의 메모리에 직접 접근할 필요가 없음.
-Map이 가지고 있는데 굳이? 그냥 자기자신의 방이 어디서부터 어디고
-몬스터는 어떻게 관리하고 이런것만 관리하면 될 듯.
+기본적인 로그라이크의 맵을 따르지 않기로 결정함.
+내가 만들 게임의 맵은 미로 형태이며, 우선적으로
+맵을 구성하는데에 필요한 객체는 Tile과 Map이면
+충분할거같음. 따라서 기존에 구상했던, Room과 Passage
+그리고 Structure는 삭제함.
+
+우선 갑자기 게임의 컨셉이 바뀐거기 때문에 여기에
+이 주석만 남겨두고, 커밋 후 브랜치 옮겨가겠음~
 */
 
 #include "Tile.h"
 
 class Map {
 private:
+	/*
 	const int width, height;
-	vector<Tile> board;
+	vector<vector<Tile *>> board;
 	vector<Room> rooms;
 	vector<Passage> passages;
 	
 	// 빈 타일들을 저장해 둔 board를 만든다.
-	void makeToEmptyBoard() {
+	void setEmptyTile() {
+		makeBoardEmpty();
+		
+		for (int h=0; h<height; h++) {
+			tmpTile.clear();
+			for (int w=0; w<width; w++) {
+				tmpTile.push_back(new Tile());
+			}
+			board.push_back(tmpTile);
+		}
+	}
+	
+	void makeBoardEmpty() {
+		// 만약 board에 값이 있다면
+		while (!board.empty()) {
+			while (!board.back().empty()) {
+				delete board.back().back();
+			}
+			board.pop();
+		}
 	}
 	
 public:
 	Map(int w, int h) : width(w), height(h) {
 		// 보드 초기화
-		createEmptyBoard();
+		makeBoardEmpty();
 	}
 	
-	// (x, y) 타일에 위치한 값을 반환한다.
+	// (x, y) 타일에 위치한 타일을 반환한다.
 	Tile* getTile(int x, int y) {}
 	
 	void generateMap() {
 		// 맵내의 방, 통로 등을 생성한다.
 	}
+	*/
 };
--- source/Passage.h
+++
@@ -1,5 +0,0 @@
-class Passage : public Structure{
-private:
-public:
-	Room (int x, int y, int w, int h): Structure(x, y, w, h) {}
-};
