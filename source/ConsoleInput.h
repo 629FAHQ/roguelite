9b748fea9040a7e5b0bc4b429d2efa3879741b8c
ssw**** <ssw3095@naver.com>
2021-11-16 13:57:30 +0000

    etc:Add comments for methods
    <description>
    To make a code easy to read, added comments for methods.
    <issue-number>

--- source/ConsoleInput.h
+++ source/ConsoleInput.h
@@ -1,37 +1,75 @@
 #include "InputInterface.h"
 #include <ncurses.h>
-/*
-W 119
-A 97
-S 115
-D 100
-SPACE 32
-ESC 27
-*/
+
 class ConsoleInput : public InputInterface {
 public:
-	ConsoleInput() : InputInterface(119, 115, 100, 97, 32, 27) {}
-	virtual int listenInput() {
-		return getch();
-	}
-	virtual void up() {
-		// 임시 구현
-		printw("up\n");
-	}
-	virtual void down() {
-		printw("down\n");
-	}
-	virtual void right() {
-		printw("right\n");
-	}
-	virtual void left() {
-		printw("left\n");
-	}
-	virtual void attack() {
-		printw("attack\n");
-	}
-	virtual void quit() {
-		printw("quit\n");
-	}
-	
+	ConsoleInput();
+	int listenInput();
+	void up();
+	void down();
+	void right();
+	void left();
+	void attack();
+	void quit();
 };
+
+/**
+	ConsoleInput의 생성자이다.
+	
+	콘솔환경에선 아스키 코드 값을 사용 하므로 인자로 해당 값들을 넘긴다.
+	각각 W, S, D, A, SPACE, ESC키의 아스키 코드이다.
+*/
+ConsoleInput::ConsoleInput() : InputInterface(119, 115, 100, 97, 32, 27) {
+	
+}
+
+/**
+	어떤 키가 입력되었는지 반환한다.
+	
+	return 입력된 키의 아스키 코드 값
+*/
+int ConsoleInput::listenInput() {
+	return getch();
+}
+
+/**
+	'위' 키에 대한 메시지를 전파한다.
+*/
+void ConsoleInput::up() {
+		printw("up\n");
+}
+
+/**
+	'아래' 키에 대한 메시지를 전파한다.
+*/
+void ConsoleInput::down() {
+		printw("down\n");
+}
+
+/**
+	'왼' 키에 대한 메시지를 전파한다.
+*/
+void ConsoleInput::left() {
+		printw("left\n");
+}
+
+/**
+	'오른' 키에 대한 메시지를 전파한다.
+*/
+void ConsoleInput::right() {
+		printw("right\n");
+}
+
+/**
+	'공격' 키에 대한 메시지를 전파한다.
+*/
+void ConsoleInput::attack() {
+		printw("attack\n");
+}
+
+/**
+	'나가기' 키에 대한 메시지를 전파한다.
+*/
+void ConsoleInput::quit() {
+		printw("quit\n");
+}
--- source/InputInterface.h
+++ source/InputInterface.h
@@ -2,48 +2,62 @@ class InputInterface {
 private:
 	const int NUM_UP, NUM_DOWN, NUM_RIGHT, NUM_LEFT, NUM_ATTACK, NUM_QUIT;
 public:
-	InputInterface(int u, int d, int r, int l, int a, int q) : NUM_UP(u), NUM_DOWN(d), NUM_RIGHT(r), NUM_LEFT(l), NUM_ATTACK(a),  NUM_QUIT(q){}
+	InputInterface(int u, int d, int r, int l, int a, int q);
 	virtual int listenInput() = 0;
-	virtual void up() {}
-	virtual void down() {}
-	virtual void right() {}
-	virtual void left() {}
-	virtual void attack() {}
-	virtual void quit() {}
-	void whatKeyIsPressed() {
-		while (1) {
-			int key = listenInput();
-			
-			if (key == NUM_UP) {
-				up();
-				break;
-			}
-			
-			if (key == NUM_DOWN) {
-				down();
-				break;
-			}
-			
-			if (key == NUM_RIGHT) {
-				right();
-				break;
-			}
-			
-			if (key == NUM_LEFT) {
-				left();
-				break;
-			}
-			
-			if (key == NUM_ATTACK) {
-				attack();
-				break;
-			}
-			
-			if (key == NUM_QUIT) {
-				quit();
-				break;
-			}
+	virtual void up() = 0;
+	virtual void down() = 0;
+	virtual void right() = 0;
+	virtual void left() = 0;
+	virtual void attack() = 0;
+	virtual void quit() = 0;
+	void whatKeyIsPressed();
+};
+/**
+	InputInterface의 생성자이다.
+	
+	param u, d, r, l, a, q
+	각각 위, 아래, 오른, 왼, 공격, 나가기 키에 대한 값을 받는다.
+*/
+InputInterface::InputInterface(int u, int d, int r, int l, int a, int q) : NUM_UP(u), NUM_DOWN(d), NUM_RIGHT(r), NUM_LEFT(l), NUM_ATTACK(a),  NUM_QUIT(q) {
+	
+}
+
+
+/**
+	어떤 키가 눌러졌는지 보고 눌러진 키에 따라 메시지를 전파한다.
+*/
+void InputInterface::whatKeyIsPressed() {
+	while (1) {
+		int key = listenInput();
+		
+		if (key == NUM_UP) {
+			up();
+			break;
 		}
 		
+		if (key == NUM_DOWN) {
+			down();
+			break;
+		}
+		
+		if (key == NUM_RIGHT) {
+			right();
+			break;
+		}
+		
+		if (key == NUM_LEFT) {
+			left();
+			break;
+		}
+		
+		if (key == NUM_ATTACK) {
+			attack();
+			break;
+		}
+		
+		if (key == NUM_QUIT) {
+			quit();
+			break;
+		}
 	}
-};
+}
